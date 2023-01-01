5e4d2cbfbd4e98facbab6a7ba728e593e76a2778
Lidor Repo <lidormalich@gmail.com>
2023-01-01 12:07:57 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,27 +1,16 @@
-function checkCar() {
-    // https://data.gov.il/he/api/3/action/datastore_search?resource_id=0866573c-40cd-4ca8-91d2-9dd2d7a492e5&limit=5000
-    var data = {
-        resource_id: '053cea08-09bc-40ec-8f7a-156f0677aff3', // the resource id
-        limit: 5, // get 5 results
-        q: 'jones' // query for 'jones'
-    };
-    $.ajax({
-        url: 'https://data.gov.il/api/3/action/datastore_search',
-        data: data,
-        dataType: 'jsonp',
-        success: function (data) {
-            alert('Total results found: ' + data.result.total)
-        }
-    });
-}
+// WORK
+// https://data.gov.il/he/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&limit=5&q=7777774
+// רכב קיים
 
 function checkInfo(carNum) {
     // let carNum = document.getElementById('carId').value;
     console.log(carNum);
+    let urlToCheck = "https://data.gov.il/he/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&limit=5&q=" + carNum;
     try {
-        fetch("https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=8266376")
+        fetch(urlToCheck)
             .then((res) => res.json()).
             then((carInfo) => {
+                console.log(urlToCheck);
                 console.log(carInfo);
                 document.getElementById('divi').innerHTML += `done
                 car num ${carNum}
